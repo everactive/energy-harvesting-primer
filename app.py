773ff42144de6e8111a443fb1ace657f1c095b94
Rachel House <rachel.house@everactive.com>
2022-11-08 14:00:20 -0500

    set up pyproject and hello world page for app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Everactive Energy Harvesting Primer", page_icon="⚡️", initial_sidebar_state="expanded"
+)
+
+st.header("Everactive Energy Harvesting Primer")
