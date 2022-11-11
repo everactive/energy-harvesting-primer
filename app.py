2f8dfd1edfd4225ba1970e0934055800376cf907
Rachel House <rachel.house@everactive.com>
2022-11-11 10:34:49 -0500

    Add simple CI config (#2)
    Add CI checks for bumped package version and code quality (currently `isort` and `black`). CI will run on PRs/commits on PRs to `main`.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Everactive Energy Harvesting Primer", page_icon="⚡️", initial_sidebar_state="expanded"
+    page_title="Everactive Energy Harvesting Primer",
+    page_icon="⚡️",
+    initial_sidebar_state="expanded",
 )
 
 st.header("Everactive Energy Harvesting Primer")
