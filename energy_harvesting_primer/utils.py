8fba60e0f181fa2d392cc254edfb50371c50abe9
Rachel House <rachel.house@everactive.com>
2023-02-02 11:13:43 -0500

    add visual community links to primer conclusion

--- energy_harvesting_primer/utils.py
+++ energy_harvesting_primer/utils.py
@@ -1,5 +1,7 @@
 """Contains helper methods for energy harvesting primer text and visuals."""
 
+import base64
+import os
 
 IDLE_DUTY_CYCLE = "Never (always idle)"
 MU = "\u03bc"
@@ -29,3 +31,34 @@ def get_runtime_seconds(
         return 1_000_000 * 60
 
     return runtime_seconds
+
+
+def get_linked_image(image_filepath: str, target_url: str, image_width: int) -> str:
+    """Returns HTML that encodes the specified image and links the target URL.
+
+    Streamlit doesn't have a native capability to hyperlink images. This function
+    returns HTML that can be used with st.markdown(html_str, unsafe_allow_html=True)
+    to display an image that is linked to the requested URL, at the requested width.
+    It's a hack until they add this as a native capability.
+
+    Args:
+        image_filepath: Filepath to local image file
+        target_url: URL to use for image hyperlink
+        image_width: Image display width, in pixels
+
+    Returns:
+        HTML, as string, that contains the appropriate <a> and <image> data
+    """
+    image_format = os.path.splitext(image_filepath)[-1].replace(".", "")
+
+    with open(image_filepath, "rb") as f:
+        image_bytes = f.read()
+    binary_image = base64.b64encode(image_bytes).decode()
+
+    html_code = f"""
+        <a href="{target_url}">
+            <img src="data:image/{image_format};base64,{binary_image}" width={image_width}/>
+        </a>
+    """
+
+    return html_code
